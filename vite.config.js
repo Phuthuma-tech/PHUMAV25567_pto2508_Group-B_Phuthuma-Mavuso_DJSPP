b29954bbeefabc8490c74b79fc253242e13bf465
Phuthuma-tech <101583880+Phuthuma-tech@users.noreply.github.com>
2026-07-19 19:05:35 +0200

    vite.config changes
    add base: process

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH ||"/PHUMAV25567_pto2508_Group-B_Phuthuma-Mavuso_DJSPP",
 });
